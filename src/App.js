97b367e583938157fe89ecd91b75ebdb76556484
bseleng <bseleng@gmail.com>
2021-05-20 10:48:45 +0300

    feat(!): add time, no render on init #3
    BREAKING CHANGE: time not displayed on init due null
    
    Took 1 hour 56 minutes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import AdMessagesWrapper from "./Components/Ad_Organisms/AdMessagesWrapper/AdMessagesWrapper";
+import AdClock from "./Components/Ad_Atoms/AdClock/AdClock";
 
 function App() {
   return (
     <div className="App">
+      <AdClock/>
       <AdMessagesWrapper btnName={"Отправить"} />
     </div>
   );
