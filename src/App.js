8d42adc9c9ba58949f58a2eaf956c82d444469a2
bseleng <bseleng@gmail.com>
2021-05-21 10:16:41 +0300

    feat(!): scroll event start to fire with ref
    #2
    
    BREAKING CHANGE: need to implement separate scrolls
    
    Took 59 minutes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import OrgMessagesWrapper from "./Components/Ad_Organisms/OrgMessagesWrapper/Org
 import AdClock from "./Components/Ad_Atoms/AtmClock/AtmClock";
 import SwipableViews from "./Components/__demos/SwipableVIews/SwipableViews";
 import PgMessageTime from "./Components/Ad_Pages/PgMessageTime/PgMessageTime";
+import {useRef, useState} from "react";
 
 function App() {
+  const [scroll, setScroll] = useState(0)
+  const scrollRef = useRef(0)
+
+  const handleScroll = () => {
+
+    // console.log('1 🐞 scrolls', scrollRef.current)
+    setScroll(scrollRef.current.scrollTop)
+    console.log('2 🐞 scrolls', scroll)
+  }
   return (
-    <div className="App">
+    <div className="App" onScroll={handleScroll} ref={scrollRef}>
       <PgMessageTime />
     </div>
   );
--- src/Components/Ad_Pages/PgMessageTime/PgMessageTime.jsx
+++ src/Components/Ad_Pages/PgMessageTime/PgMessageTime.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import OrgMessagesWrapper from "../../Ad_Organisms/OrgMessagesWrapper/OrgMessagesWrapper";
 import classes from "./PgMessageTime.module.css";
 import SwipeableViews from "react-swipeable-views";
 import AtmClock from "../../Ad_Atoms/AtmClock/AtmClock";
 
-const PgMessageTime = () => (
-  <SwipeableViews>
-    <div className={Object.assign({}, classes.Base)}>
-      <OrgMessagesWrapper btnName={"Отправить"} />
+const PgMessageTime = () => {
+  const [yOffset, setYOffset] =  useState(0)
+  const getYOffset = () => {
+    console.log('🐞 window.scrollY', window.scrollY)
+  }
+
+
+  return (
+    <div >
+      <SwipeableViews ignoreNativeScroll={true}>
+        <div className={Object.assign({}, classes.Base)}>
+          <OrgMessagesWrapper btnName={"Отправить"} onScroll={getYOffset}/>
+        </div>
+        <div className={Object.assign({}, classes.Base)}>
+          Текущее время
+          <AtmClock/>
+        </div>
+        <div className={Object.assign({}, classes.Base)}>
+          Текущее время 2
+          <AtmClock/>
+        </div>
+      </SwipeableViews>
+
     </div>
-    <div className={Object.assign({}, classes.Base)}>
-      Текущее время
-      <AtmClock/>
-    </div>
-  </SwipeableViews>
-);
+  )
+};
 
 export default PgMessageTime;
