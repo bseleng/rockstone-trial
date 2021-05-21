6447f5466e155fb9a3ed9bf9e4a05738bf9fe3ed
bseleng <bseleng@gmail.com>
2021-05-21 08:34:12 +0300

    feat: add working clock to second view #2
    Took 9 minutes

---
+++ src/Components/AD_Atoms/AtmClock/AtmClock.jsx
@@ -0,0 +1,43 @@
+import React, {useRef, useState} from "react";
+
+const AtmClock = () => {
+  const dateNow = new Date()
+
+  const timeRef = useRef(null)
+  const [time, setTime] = useState(timeRef.current)
+
+
+  const addForwardZero = timeSegment => {
+    return timeSegment.toString().padStart(2,'0')
+  }
+  const getSeconds = () => {
+    return addForwardZero(dateNow.getSeconds())
+  }
+  const getMinutes = () => {
+    return addForwardZero(dateNow.getMinutes())
+  }
+  const getHours = () => {
+    return addForwardZero(dateNow.getHours())
+  }
+  const getCurrentTime = () => {
+    return `${getHours()} : ${getMinutes()} : ${getSeconds()}`
+  }
+
+
+  timeRef.current = getCurrentTime()
+  setInterval(() => {
+    setTime(seconds=> timeRef.current)
+  }, 1000)
+
+  if(!time) {
+    return null
+  }
+
+  return (
+    <>
+    <div> {time}</div>
+
+    </>
+  )
+}
+export default AtmClock
--- src/Components/Ad_Pages/PgMessageTime/PgMessageTime.jsx
+++ src/Components/Ad_Pages/PgMessageTime/PgMessageTime.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import OrgMessagesWrapper from "../../Ad_Organisms/OrgMessagesWrapper/OrgMessagesWrapper";
 import classes from "./PgMessageTime.module.css";
 import SwipeableViews from "react-swipeable-views";
+import AtmClock from "../../Ad_Atoms/AtmClock/AtmClock";
 
 const PgMessageTime = () => (
   <SwipeableViews>
     <div className={Object.assign({}, classes.Base)}>
       <OrgMessagesWrapper btnName={"Отправить"} />
     </div>
-    <div className={Object.assign({}, classes.Base)}> it's a clock</div>
+    <div className={Object.assign({}, classes.Base)}>
+      Текущее время
+      <AtmClock/>
+    </div>
   </SwipeableViews>
 );
 
